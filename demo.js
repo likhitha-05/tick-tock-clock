462a18911b8ca928b3eadbb016f409bf686da6a8
Banothu likhitha <banothu.likhitha@ZL-BLR-MAC540.local>
2023-06-09 14:20:54 +0530

    second commit

---
+++ demo.js
@@ -0,0 +1,17 @@
+/*variable declaration: let,const,and var
+let: can be reinitialize
+const: cannot be reinitialized
+var: 
+*/
+
+let fname="likhitha";
+console.log(fname);
+fname="Banothu";
+console.log(fname);
+
+const lname="likhitha";
+console.log(lname);
+lname="Banothu";
+console.log(lname);
+
+

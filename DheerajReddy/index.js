04e3fad389748f28a26a3e4c7a4c12e8d63c7664
Dheeraj <dheeraj.reddy@zee.com>
2023-06-12 14:22:27 +0530

    second commit

--- DheerajReddy/index.js
+++ DheerajReddy/index.js
@@ -1,13 +1,13 @@
 setInterval(() => {
-	d = new Date(); //object of date()
-	hr = d.getHours();
-	min = d.getMinutes();
-	sec = d.getSeconds();
-	hr_rotation = 30 * hr + min / 2; //converting current time
-	min_rotation = 6 * min;
-	sec_rotation = 6 * sec;
+	date = new Date(); 
+	hr = date.getHours();
+	min = date.getMinutes();
+	sec = date.getSeconds();
+	hr_hand= 30 * hr + min / 2;
+	min_hand= 6 * min;
+	sec_hand = 6 * sec;
 
-	hour.style.transform = `rotate(${hr_rotation}deg)`;
-	minute.style.transform = `rotate(${min_rotation}deg)`;
-	second.style.transform = `rotate(${sec_rotation}deg)`;
+	hour.style.transform = `rotate(${hr_hand}deg)`;
+	minute.style.transform = `rotate(${min_hand}deg)`;
+	second.style.transform = `rotate(${sec_hand}deg)`;
 }, 1000);
